171461568ae6804292875527952afc384507800b
emericgautier <emericgtr@gmail.com>
2022-07-25 11:18:44 +0200

    scroll event

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY < lastScroll) {
+    // console.log(navbar);
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = "-60px";
+  }
+
+  lastScroll = window.scrollY;
+});
+
 // Créer un événément au scroll
 
-// Cacher la navbar si l'utilisateur comment à descendre et la sortir quand il remonte
+// Cacher la navbar si l'utilisateur commence à descendre et la sortir quand il remonte
 
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
